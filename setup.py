ade6a7b0eb1d2b0e7150a424d86c9bb35bb3c712
Tristram Gräbener <tristramg@gmail.com>
2010-06-16 16:28:18 +0200

    Corrected include_dirs
    It could have an empty value, that would result in -I -I/foo/blah
    This could lead to a failed compilation

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+# Update this if needed
+# It has to point to the directory containing the boost headers
+boost_path = "."
+
+
 from distribute_setup import use_setuptools
 use_setuptools()
 
 from setuptools import setup, find_packages, Extension
 setup(name='Osm4routing',
       version='1.0',
-      author= 'Tristram',
+      author= 'Tristram Graebener',
       author_email = 'tristramg@gmail.com',
+      description = 'A simple tool to parse OpenStreetMap data to use them for routing',
+      license = 'GPLv3',
       url = 'http://github.com/Tristramg/osm4routing/',
       install_requires = ['progressbar', 'sqlalchemy', 'setuptools-git'],
       py_modules = ['osm4routing', 'osm4routing_xml'],
@@ -14,7 +21,7 @@ setup(name='Osm4routing',
           Extension("_osm4routing_xml",
               sources=["parse.cc", "parameters.cc", "parse.i"],
               swig_opts=['-c++'],
-              include_dirs=['.'],
+              include_dirs=[boost_path],
               libraries=['expat'])
           ],
        entry_points = {
